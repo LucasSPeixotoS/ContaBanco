4752ccdbc7b25af0729592ae857714314b0180f0
Lucas <lucasxvs5@gmail.com>
2024-09-05 12:36:30 -0300

    Criação da lógica do desafio

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,7 +1,34 @@
+import java.math.BigDecimal;
+import java.util.Random;
+import java.util.Scanner;
+
 public class ContaTerminal {
+
+    public static String agency;
+    public static String customerName;
+    public static Random random = new Random();
+    public static int number = random.nextInt(10000);
+    public static double bankBalance = random.nextDouble(10000);
+    public static String formattedBankBalance = String.format("%.2f", bankBalance);
+
     public static void main(String[] args) {
 
+        Scanner reader = new Scanner(System.in);
 
+        System.out.println("Bem vindo ao banco Dinheiro Certo!\n\nPor favor, digite o número da sua Agência!");
+
+        agency = reader.nextLine();
+
+        System.out.println("Digite o seu nome completo.");
+
+        customerName = reader.nextLine();
+
+        information();
 
     }
+
+    public static void information(){
+        System.out.println("Olá " + customerName + ", obrigado por criar uma conta em nosso banco, sua agência é " + agency + ", conta " + number + " e seu saldo " + formattedBankBalance + " já está disponível para saque");
+    }
+
 }
